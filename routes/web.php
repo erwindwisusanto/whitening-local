044cb05f7dea5b8ff9a1f96309b5ad93588e8286
erwin dwi s <erwin@waki.mobi>
2024-07-02 17:20:30 +0700

    xx

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::post('/destroy-session', [MainController::class, 'destroySession'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
 });
 
-Route::post('/save-visit-count', [MainController::class, 'saveVisitCount'])->name('visit-count');
+Route::post('/save-visit-count', [MainController::class, 'saveVisitCount'])->name('visit-count')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
 Route::post('/save-click-count', [MainController::class, 'saveClickCount'])->name('click-count');
 Route::get('/wa-wording', [MainController::class, 'getWaWording'])->name('get-wa-wording');
 
