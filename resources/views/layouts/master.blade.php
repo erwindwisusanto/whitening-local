fb5220b048eaa3e4d885d9a49fef6b37030c23ad
erwin <erwin@waki.mobi>
2024-09-23 10:29:26 +0700

    feat: add GTM

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,14 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Whitening - Cepat Sehat</title>
 
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PCSK6F8L');
+    </script>
+
+
     <!-- icons -->
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.2.96/css/materialdesignicons.min.css"
@@ -33,6 +41,9 @@
 </head>
 
 <body>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCSK6F8L"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
     <x-navbar/>
 
     {{ $slot }}
